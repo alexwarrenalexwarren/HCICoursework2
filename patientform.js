27dc7ed92fd60c9e7f5d8621299bd7ff74b97513
Alex Warren <80273931+alexwarrenalexwarren@users.noreply.github.com>
2021-12-14 12:48:54 +0000

    Final Version

---
+++ patientform.js
@@ -0,0 +1,21 @@
+window.onscroll = function () { stickyHeader() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function dropDown() {
+    var x = document.getElementById("myHeader");
+    if (x.className === "header") {
+        x.className += " responsive";
+    } else {
+        x.className = "header";
+    }
+}
